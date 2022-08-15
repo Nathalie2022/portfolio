d2b607a6e636fe990d9e5337f04024f082b72680
Nathalie Le Roux <nathalie.lerouw@gmx.fr>
2022-08-15 12:23:01 +0200

    nav

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ window.onscroll=function(){
         scrollTopButton.classList.remove("show")
     }
 }
+
+const navbar = document.getElementById("navbarSite");
+
+window.onscroll=function(){
+    if(window.pageYOffset>200){
+        navbar.classList.remove("bg-transparent");
+        navbar.classList.remove("navbar-dark");
+        navbar.classList.add("bg-light");
+        navbar.classList.add("navbar-light");
+    }
+    else{
+        navbar.classList.add("bg-transparent");
+        navbar.classList.add("navbar-dark");
+        navbar.classList.remove("bg-light")
+        navbar.classList.remove("navbar-light")
+    }
+}
